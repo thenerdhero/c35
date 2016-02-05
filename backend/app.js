9cc091612f28353a3661703957de8bf7ba4237ef
Dan LaFeir <dlafeir@thoughtworks.com>
2016-02-05 08:26:56 -0600

    Dan [] Added example of adding css/js

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,7 @@ var app = express();
 //setup templating for rendering files in the views folder in frontend/
 app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname.replace('backend', 'frontend') + '/views');
-
-var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/icstars';
-
-var client = pg.connect(connectionString, function(){});
+app.use(express.static(__dirname.replace('backend', 'frontend')));
 
 app.get('/', function(req,res){
   return res.render('index.html');
