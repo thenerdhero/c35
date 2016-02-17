1c4a4d4f8e569730e6f77e3dc1bd2402853fc862
Dan LaFeir <dlafeir@thoughtworks.com>
2016-02-16 23:28:59 -0500

    Dan [setting up router] added instructions for setting up GET request

--- backend/router.js
+++ backend/router.js
@@ -1,13 +1,17 @@
 var router = require('express').Router();
- 
+/*
+ Here is where you will configure the routes of your application.
+ This how you can make urls that will render your html pages you put in the views folder.
+ To do this you need to add:
+  router.get('string with your route', function(req,res){
+    return res.render('path to your html in the views folder')
+  });
+
+ */
 module.exports = function(){
 
   router.get('/helloworld', function(req,res){
-    return res.render('index.html');
-  });
-
-  router.post('/tableOfResults', function(req,res){
-    return res.render('tableOfResults.html');
+    return res.render('helloworld.html');
   });
 
   return router
