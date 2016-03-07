319fc2e35cbd4f7edae26e7e19382ea7d8f76b5b
icstars <appldev@icstars.org>
2016-03-07 17:56:15 +0000

    added full script to create the database and the readonly user to connect

--- backend/postgres_trains_example.sql
+++ backend/postgres_trains_example.sql
@@ -5,9 +5,37 @@
 -- export PGPASSWORD=<password>
 -- psql -h <host> -d <database> -U <user_name> -p <port> <path to postgres_trains_example.sql file>
 
+-- in cloud 9, start the postgres service:
+-- sudo service postgresql start
+
+-- connect:
+-- sudo sudo -u postgres psql
+
+-- get to the filesystem while still logged in as super user
+-- \! 
+
+-- change this directory
+-- cd /backend
+
+-- run this file
+-- psql -f postgres_trains_example.sql
+
+create database nodeworkshop;
+
+\c nodeworkshop;
+
 create table if not exists trains (
   id serial primary key,
   name text,
   inService boolean,
   numberOfAvailable int
 );
+
+create role nodeuser with password '13149700' login;
+grant connect on database nodeworkshop to nodeuser;
+grant select on public.trains to nodeuser;
+
+insert into trains (name, inService, numberOfAvailable) values ('redline 813', true, '8');
+insert into trains (name, inService, numberOfAvailable) values ('redline 912', true, '10');
+insert into trains (name, inService, numberOfAvailable) values ('greenline 713', true, '10');
+insert into trains (name, inService, numberOfAvailable) values ('brownline 412', true, '8');
