f5d8d7daf0504ffaf2ef251979fa76910fe86ab5
ikorchynska <ikorchynska@icstars.org>
2016-04-01 18:02:50 +0000

    Iryna:last changes with link

---
+++ nrp.js
@@ -0,0 +1,15 @@
+/*'use strict'
+
+$(document).ready(function(){
+
+  $('#addCourseForm').submit(function(){
+
+    $.post('/nrp', {
+      courseName: $('#courseName').val(),
+    });
+
+    return false;
+  });
+
+});
+ */
